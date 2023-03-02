09b32f9f1450c9752e5c120820c7328ffae0044b
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-02 14:42:05 -0500

    Add place for image. Add image to public folder.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,34 +4,39 @@ import './about.css';
 const About = () => {
   return (
     <a id="about">
-        <div className="heading">
-          <p class="h4">About Me</p>
-        </div>
-        <div className="content">
-          <p>Hello, my name is Zachary Blum and I am a current UCF Honors Computer Science Major.
-             I'm interested in full-stack development, with experience in frontend development. 
-             I also have an interest in project management, acting as one in multiple group projects
-             and using the Agile framework.
-          </p>
-          <p>Here are some technologies I've been working with recently.</p>
-          <div class="container">
-            <div class="row">
-              <div class="col-sm">
-                <p>JavaScript</p>
+        <div id="info">
+          <div className="heading">
+            <p class="h4">About Me</p>
+          </div>
+          <div className="content">
+            <p>Hello, my name is Zachary Blum and I am a current UCF Honors Computer Science Major.
+              I'm interested in full-stack development, with experience in frontend development. 
+              I also have an interest in project management, acting as one in multiple group projects
+              and using the Agile framework.
+            </p>
+            <p>Here are some technologies I've been working with recently:</p>
+            <div class="container">
+              <div class="row">
+                <div class="col-sm">
+                  <p>JavaScript</p>
+                </div>
+                <div class="col-sm">
+                  <p>React</p>
+                </div>
               </div>
-              <div class="col-sm">
-                <p>React</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm">
-                <p>C++</p>
-              </div>
-              <div class="col-sm">
-                <p>Java</p>
+              <div class="row">
+                <div class="col-sm">
+                  <p>C++</p>
+                </div>
+                <div class="col-sm">
+                  <p>Java</p>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div id="img">
+          <img src='/aboutme.jpg' alt="Place image" />
         </div>
     </a>
   )
