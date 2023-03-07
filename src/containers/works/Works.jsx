a06f3e3fe314d29d66008c2105175f8be5a165da
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-06 19:09:52 -0500

    Create basic project data, and map it for featured projects.

--- src/containers/works/Works.jsx
+++ src/containers/works/Works.jsx
@@ -4,12 +4,33 @@ import './works.css';
 import { FeaturedProject } from '../../components';
 import {OtherProjects} from '../../containers';
 
+const fps = [
+    {
+        title: "Contact Manager", role: "Project Manager and Frontend Developer", 
+        description: "A web app for tracking a users contacts.",
+        responsibilites: ["Created a UML Use Case Diagram and Entity Relationship Diagram", 
+                            "Created Kanban board and Gantt chard to organize the build and track team performance",
+                            "Built the landing, login and signup pages using HTML, CSS and JavaScript"],
+        technologies: ["HTML", "CSS", "JavaScript"],
+        complete: true
+    },
+    {
+        title: "Household", role: "Project Manager and Frontend Developer",
+        description: "A web and mobile app for tracking household tasks, chores and events.",
+        responsibilites: ["Created and managed Jira project using Agile development", 
+                            "Taught members of team basic React"],
+        technologies: ["Jira", "React", "Styled Components", "Agile"],
+        complete: false
+    },
+]
+
 const Works = () => {
   return (
     <a id="works"> 
-        <FeaturedProject/>
-        <FeaturedProject/>
-        <FeaturedProject/>
+        {
+        fps.map((fp) => <FeaturedProject title={fps.title} description={fps.description} 
+        resp={fps.responsibilites} tech={fps.technologies} complete={fps.complete}/>)
+        }
         <OtherProjects/>
     </a>
   )
