88b0446e95025ab339269ce4d431e02acd8a7ca1
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-20 14:23:16 -0400

    Adjusted grouping and spacing of about me section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,36 +10,38 @@ const About = () => {
           <div className="heading">
             <p class="h4">About Me</p>
           </div>
-          <div className="content">
-            <p>Hello, my name is Zachary Blum and I am a current UCF Honors Computer Science Major.
-              I'm interested in full-stack development, with experience in frontend development. 
-              I also have an interest in project management, acting as one in multiple group projects
-              and using the Agile framework.
-            </p>
-            <p>Here are some technologies I've been working with recently:</p>
-            <div class="container">
-              <div class="row">
-                <div class="col-sm">
-                  <p>JavaScript</p>
+          <div class='contain'>
+            <div className="content">
+              <p>Hello, my name is Zachary Blum and I am a current UCF Honors Computer Science Major.
+                I'm interested in full-stack development, with experience in frontend development. 
+                I also have an interest in project management, acting as one in multiple group projects
+                and using the Agile framework.
+              </p>
+              <p>Here are some technologies I've been working with recently:</p>
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm">
+                    <p>JavaScript</p>
+                  </div>
+                  <div class="col-sm ">
+                    <p>React</p>
+                  </div>
                 </div>
-                <div class="col-sm ">
-                  <p>React</p>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm">
-                  <p>C++</p>
-                </div>
-                <div class="col-sm">
-                  <p>Java</p>
+                <div class="row">
+                  <div class="col-sm">
+                    <p>C++</p>
+                  </div>
+                  <div class="col-sm">
+                    <p>Java</p>
+                  </div>
                 </div>
               </div>
             </div>
+            <div id="image">
+              <img src={PF} alt="Profile Picture" style={{color: "white"}} class="rounded"/>
+            </div>
           </div>
-        </div>
-        <div id="image">
-          <img src={PF} alt="Profile Picture" style={{color: "white"}} class="rounded"/>
-        </div>
+          </div>
     </a>
   )
 }
